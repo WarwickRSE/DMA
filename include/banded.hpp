14398ed71b9a650472f8cad8a80d131eb4d1b419
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-05-24 19:51:00 +0100

    Combine repeating and not

--- include/banded.hpp
+++ include/banded.hpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include <iostream>
 #include <cassert>
+#include <type_traits>
 
 namespace elementary_matrices{
 
@@ -101,25 +102,38 @@ class general_mat{
     }
 };
 
-template <int bandw> 
+template <int bandw, bool repeating=false>
 class banded_general{
+
+  typedef typename std::conditional<repeating, int, const int>::type int_c;
+  typedef typename std::conditional<repeating, const int, int>::type int_c2;
 
   std::vector< std::vector<double> > values;
 
-  public:
-  const int len;
+  inline int reps()const{return len - hdr - ftr;}
 
-  banded_general(const int len_in):len(len_in){
-    // Checks
+  public:
+  int_c len; // Variable for repeating, const for not
+  int_c2 hdr, ftr; // Const for repeating, redundant for not
+
+  banded_general(const int len_in, const int hdr_in=0, const int ftr_in=0):len(len_in), hdr(hdr_in), ftr(ftr_in){
+  // Checks
 #ifdef DEBUG
-    assert(bandw % 2 == 1); // Bandwidth has to be odd (main diag plus 2*off-diag)
+    static_assert(bandw % 2 == 1); // Bandwidth has to be odd (main diag plus 2*off-diag)
     assert(len > bandw); // Doesn't make any sense otherwise...
+    assert(!repeating || len > hdr+ftr+1); // Must have at least one repeating row if repeating
 #endif 
 
     const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
     values.resize(bandw);
     for(int i = 0; i < bandw; ++i){
-        int l = len - std::abs(i-centr);
+        int l;
+        if constexpr(repeating){
+          l = hdr + ftr + 1;
+        }else{
+          l = len;
+        }
+        l -= std::abs(i-centr);
         values[i].resize(l);
     }
   }
@@ -129,6 +143,17 @@ class banded_general{
             value = 0.0;
         }
     }
+  }
+  void change_len(int new_len){
+    // Checked way to change the length. For unchecked, just do it
+    if constexpr(!repeating){
+      static_assert(repeating, "Cannot change length of non-repeating matrix");
+    }
+#ifdef DEBUG
+    assert(new_len > hdr+ftr+1); // Must have at least one repeating row
+    assert(new_len > bandw); // Doesn't make any sense otherwise...
+#endif
+    len = new_len;
   }
 
   void fill_from_otherstuff(){ //TODO
@@ -140,8 +165,10 @@ class banded_general{
     const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
     const int elem_sz = std::sqrt(elem.size()); // Size of elementary
     const int elem_bw = elem_sz * 2 - 1;
+    assert(!repeating || elem_sz-1 <= hdr);// Else the elementary matrix will not repeat correctly
 
-    for(int j = 0; j < len - elem_sz+1; j++){
+    const int stored_len = values[centr].size();
+    for(int j = 0; j < stored_len - elem_sz+1; j++){
       int offset = 0;
       for(int e = 0; e < elem_sz; e++){
         for(int i = e; i < bandw-e; i++){
@@ -168,6 +195,9 @@ class banded_general{
 
   bool set_identity_row(int row){
     // Allow for any row
+    if constexpr (repeating){
+      if(row < 0 || row >= hdr) return false;
+    }
     const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
     for(int i = 0; i < bandw; i++){
         int off = centr - i;
@@ -184,40 +214,64 @@ class banded_general{
   }
 
   void print()const{
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
     for(int i =0; i < bandw; i++){
         for(int j = 0; j < values[i].size(); j++ ){
             std::cout<< values[i][j]<<"\t";
+            if(repeating && j == hdr) std::cout<<":\t";
         }
         std::cout<<'\n';
     }
   }
-  void pretty_print()const{print();}
+
+  void pretty_print()const{
+    if constexpr(!repeating){
+      print();
+    }else{
+      const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
+      for(int i = 0; i < bandw; i++){
+        int l = len - std::abs(i-centr); // Real diagonal length, not stored
+          for(int j = 0; j < l; j++){
+              std::cout<<get(i, j)<<"\t";
+          }
+          std::cout<<'\n';
+      }
+    }
+  }
 
   void expanded_print(bool full = false)const{
-    // Print as full matrix. Sig to match repeating one
+    // Print as full matrix
     for(int i = 0; i < len; i++){
+      bool print_row = !repeating || (full || i < hdr + 1 || i > len - ftr - 3);
+      if(print_row){
         for(int j = 0; j < len; j++){
           std::cout<<get_real(i, j)<<"\t";
         }
         std::cout<<'\n';
+      }else if(repeating && i == hdr + 1){
+        std::cout<<"........\n";
+      }
     }
   }
 
   double get(int i, int j) const{
-    // Get value in diagonal i, position j
+    // Get value in diagonal i, position j. j can run from 0 to len - l where l is the number of the diagonal
 #ifdef DEBUG
     if(i >= bandw || j >= len) throw std::out_of_range("Out of range banded get");
 #endif
-    return values[i][j];
+    if constexpr(repeating){
+      return values[i][(j < hdr ? j :(j > len-ftr-1 ? (j-reps()+1) : hdr))];
+    }else{
+      return values[i][j];
+    }
   }
-  void set(int i, int j, double val){
-    // Set value in diagonal i, position j
-#ifdef DEBUG
-    if(i >= bandw || j >= len) throw std::out_of_range("Out of range banded set");
- #endif
-    values[i][j] = val;
-  }
+  /* NOTE: that horror show is the inline replication of:
+     if(j < hdr){
+        j;
+    }else if(j > len - ftr - 1){
+        j - reps+1; // End bit
+    }else{
+        hdr; // Repeating bit - note hdr is length of hdr section, so is correct index
+    */
 
   double get_real(int i, int j)const{
     // Get value at _real_ indices i, j
@@ -228,7 +282,23 @@ class banded_general{
       return 0.0;
     }
   }
+
+// Set values only for non-repeating matrices
+  void set(int i, int j, double val){
+    if constexpr(repeating){
+      static_assert(!repeating, "Cannot set values in repeating matrix");
+    }
+    // Set value in diagonal i, position j
+#ifdef DEBUG
+    if(i >= bandw || j >= len) throw std::out_of_range("Out of range banded set");
+ #endif
+    values[i][j] = val;
+  }
+
   void set_real(int i, int j, double val){
+    if constexpr(repeating){
+      static_assert(!repeating, "Cannot set values in repeating matrix");
+    }
     // Set value at _real_ indices i, j
     index_helper inds = index_from_real<bandw>(len, i, j);
     if(inds.valid){
@@ -240,221 +310,12 @@ class banded_general{
     }
   }
 
-  general_mat to_general()const{
-    // Expand into a normal matrix form
-    auto tmp = general_mat(len);
-    for(int i = 0; i < len; i++){
-        for(int j = 0; j < len; j++){
-          tmp.set(i, j, get_real(i, j));
-        }
+   banded_general<bandw, false> to_full()const{
+    if constexpr(!repeating){
+      // No-op
+      return *this;
     }
-    return tmp;
-  }
-
-  bool operator==(const banded_general & other) const{
-    if(len != other.len) return false;
-    for(int i = 0; i < bandw; i++){
-      for(int j = 0; j < values[i].size(); j++){
-        if(values[i][j] != other.values[i][j]) return false;
-      }
-    }
-    return true;
-  }
-  friend bool operator==(const general_mat & gen, const banded_general & banded){
-    if(gen.len != banded.len) return false;
-    for(int i = 0; i < gen.len; i++){
-      for(int j = 0; j < gen.len; j++){
-        // Also checks for all out-of-band elements being zero because of getter implementation
-        if(gen.get(i, j) != banded.get_real(i, j)) return false;
-      }
-    }
-    return true;
-  }
-  friend bool operator==(const banded_general & banded, const general_mat & gen){
-    return gen == banded;
-  }
-
-};
-
-template <int bandw>
-class banded_repeating{
-// A banded matrix where the middle rows are identical - saves storing them all
-// The number of start and end rows is fixed, but the length is allowed to vary by
-// changing the number of repeats
-
-  private:
-  std::vector< std::vector<double> > values;
-
-  public:
-  int len;
-  const int hdr; // Number of initial rows
-  const int ftr; // Number of final rows
-
-  inline int reps()const{return len - hdr - ftr;}
-
-  banded_repeating(const int len_in, const int hdr_in, const int ftr_in):len(len_in), hdr(hdr_in), ftr(ftr_in){
-    // Checks
-#ifdef DEBUG
-    assert(bandw % 2 == 1); // Bandwidth has to be odd (main diag plus 2*off-diag)
-    assert(len > bandw); // Doesn't make any sense otherwise...
-    assert(len > hdr+ftr+1); // Must have at least one repeating row
-#endif 
-
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
-    values.resize(bandw);
-    int stored_len = hdr + ftr + 1; // Length required to store elements
-    for(int i = 0; i < bandw; ++i){
-        int l = stored_len - std::abs(i-centr);
-        values[i].resize(l);
-    }
-  }
-  void clear(){
-    for(int i = 0; i < bandw; i++){
-        for(double & value : values[i]){
-            value = 0.0;
-        }
-    }
-  }
-  void change_len(int new_len){
-#ifdef DEBUG
-    assert(new_len > hdr+ftr+1); // Must have at least one repeating row
-    assert(new_len > bandw); // Doesn't make any sense otherwise...
-#endif
-    len = new_len;
-  }
-
-  void fill_from_otherstuff(){ //TODO
-  };
-
-
-  void fill_from_elemBanded(const std::vector<double> elem){
-    // ADDS the elementary banded array contributions
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
-
-    const int elem_sz = std::sqrt(elem.size()); // Size of elementary
-    const int elem_bw = elem_sz * 2 - 1;
-    assert(elem_sz-1 <= hdr);
-
-    const int stored_len = values[centr].size();
-
-    for(int j = 0; j < stored_len - elem_sz+1; j++){
-      int offset = 0;
-      for(int e = 0; e < elem_sz; e++){
-        for(int i = e; i < bandw-e; i++){
-          values[i][j+e] += elem[offset + i];
-        }
-        offset += bandw - e*2 - 1; 
-      }
-    }
-  }
-  void multiply_all(double factor){
-    for(int i = 0; i < bandw; i++){
-        for(double & value : values[i]){
-            value *= factor;
-        }
-    }
-  }
-  void add_identity_factor(double factor){
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
-    const int stored_len = values[centr].size();
-
-    for(int i = 0; i < stored_len; i++){
-        values[centr][i] += factor;
-    }
-  }
-
-  bool set_row(int row, std::vector<double> vals){
-    if(row < 0 || row >= hdr) return false;
-    values[row] = vals;
-    return true;
-  }
-  bool set_identity_row(int row){
-    // Only allowed to manipulate initial rows
-    if(row < 0 || row >= hdr) return false;
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
-    for(int i = 0; i < bandw; i++){
-        int off = centr - i;
-        if(off == 0){
-          values[i][row] = 1.0;
-        }
-        else if(off > 0 && row-off >= 0){
-            values[i][row-off] = 0.0;
-        }else if(off < 0){
-            values[i][row] = 0.0;
-        }
-    }
-    return true;
-  }
-
-  void print()const{
-    for(int i =0; i < bandw; i++){
-        for(int j = 0; j < values[i].size(); j++ ){
-            std::cout<< values[i][j]<<"\t";
-            if(j == hdr) std::cout<<":\t";
-        }
-        std::cout<<'\n';
-    }
-  }
-
-  inline int get_second(int j)const{
-    // Get the index for stored values corresponding to value j in actual diagonal
-    return (j < hdr ? j :(j > len-ftr-1 ? (j-reps()+1) : hdr));
-    /* Inline replication of:
-     if(j < hdr){
-        j;
-    }else if(j > len - ftr - 1){
-        j - reps+1; // End bit
-    }else{
-        hdr; // Repeating bit - note hdr is length of hdr section, so is correct index
-    */
-  }
-
-  double get(int i, int j) const{
-    // Return the value at position j on diagonal i. j can run from 0 to len - l where l is the number of the diagonal
-#ifdef DEBUG
-    if(i >= bandw || j >= len) throw std::out_of_range("Out of range");
-#endif
-    return values[i][get_second(j)];
-  }
-
-  double get_real(int i, int j)const{
-    // Get value at _real_ indices i, j
-    index_helper inds = index_from_real<bandw>(len, i, j);
-    if(inds.valid){
-      return get(inds.i, inds.j);
-    }else{
-      return 0.0;
-    }
-  }
-
-  void pretty_print()const{
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
-    for(int i = 0; i < bandw; i++){
-      int l = len - std::abs(i-centr); // Real diagonal length, not stored
-        for(int j = 0; j < l; j++){
-            std::cout<<get(i, j)<<"\t";
-        }
-        std::cout<<'\n';
-    }
-  }
-  void expanded_print(bool full = false)const{
-    // Print as full matrix
-    // full flag means print all the repeating rows too
-    const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
-    for(int i = 0; i < len; i++){
-      if(full || i < hdr + 1 || i > len - ftr - 3){
-        for(int j = 0; j < len; j++){
-            std::cout<<get_real(i, j)<<"\t";
-        }
-        std::cout<<'\n';
-      }else if(i == hdr + 1){
-            std::cout<<"........\n";
-      }
-    }
-  }
-
-  banded_general<bandw> to_full()const{
-    auto tmp = banded_general<bandw>(len);
+    auto tmp = banded_general<bandw, false>(len);
     for(int i = 0; i < bandw; i++){
         for(int j = 0; j < len; j++){
           tmp.set(i, j, get(i, j));
@@ -474,44 +335,46 @@ class banded_repeating{
     return tmp;
   }
 
-  friend bool operator==(const banded_repeating & lhs,  const banded_general<bandw> & rhs){
+  friend bool operator==(const banded_general & lhs, const banded_general & rhs){
+    if(lhs.len != rhs.len) return false;
+    for(int i = 0; i < bandw; i++){
+      // Only need to check the stored values
+      for(int j = 0; j < lhs.values[i].size(); j++){
+        if(lhs.values[i][j] != rhs.values[i][j]) return false;
+      }
+    }
+    return true;
+  }
+  //Comparisons with other value for repeating...
+  friend bool operator==(const banded_general<bandw, repeating> & lhs,  const banded_general<bandw, !repeating> & rhs){
     // Check for equality without repeats
     const int centr = (bandw + 1)/2 - 1; // -1 for 0-indexing
     if(lhs.len != rhs.len) return false;
     for(int i = 0; i < bandw; i++){
       int l = lhs.len - std::abs(i-centr);
       for(int j = 0; j < l; j++ ){
-        // Have to check every value in other across repeating section
+        // Have to check every value in repeating section
         if(lhs.get(i, j) != rhs.get(i, j)) return false;
       }
     }
     return true;
   }
-  friend bool operator==(const banded_general<bandw> & lhs, const banded_repeating & rhs){
-    return rhs == lhs;
-  }
-  friend bool operator==(const general_mat & gen, const banded_repeating & banded){
+
+  //Equality with full matrices
+  friend bool operator==(const general_mat & gen, const banded_general & banded){
     if(gen.len != banded.len) return false;
     for(int i = 0; i < gen.len; i++){
       for(int j = 0; j < gen.len; j++){
+        // Also checks for all out-of-band elements being zero because of getter implementation
         if(gen.get(i, j) != banded.get_real(i, j)) return false;
       }
     }
     return true;
   }
-  friend bool operator==(const banded_repeating & banded, const general_mat & gen){
+  friend bool operator==(const banded_general & banded, const general_mat & gen){
     return gen == banded;
   }
 
-  bool operator==(const banded_repeating & other) const{
-    if(len != other.len) return false;
-    for(int i = 0; i < bandw; i++){
-      for(int j = 0; j < values[i].size(); j++){
-        if(values[i][j] != other.values[i][j]) return false;
-      }
-    }
-    return true;
-  }
 };
 
 general_mat matmul(const general_mat & L, const general_mat & U){
@@ -529,8 +392,8 @@ general_mat matmul(const general_mat & L, const general_mat & U){
   return result;
 }
 
-template <int bandw>
-general_mat matmul(const banded_general<bandw> & L, const banded_general<bandw> & U){
+template <int bandw, bool repeatinga, bool repeatingb>
+general_mat matmul(const banded_general<bandw, repeatinga> & L, const banded_general<bandw, repeatingb> & U){
   // Multiply two banded matrices. Note that in general the result is NOT banded, so only a general matrix is returned
   assert(L.len == U.len);
   general_mat result(L.len);
@@ -547,24 +410,7 @@ general_mat matmul(const banded_general<bandw> & L, const banded_general<bandw> 
 }
 
 template <int bandw>
-general_mat matmul(const banded_repeating<bandw> & L, const banded_repeating<bandw> & U){
-  // Multiply two repeating matrices. Note that in general the result is NOT banded, so only a general matrix is returned
-  assert(L.len == U.len);
-  general_mat result(L.len);
-  for(int i = 0; i < L.len; i++){
-    for(int j = 0; j < L.len; j++){
-      double tmp = 0.0;
-      for(int k = 0; k < L.len; k++){
-        tmp += L.get_real(i, k)*U.get_real(k, j);
-      }
-      result.set(i, j, tmp);
-    }
-  }
-  return result;
-}
-
-template <int bandw>
-banded_general<bandw> from_general(const general_mat & gen){
+banded_general<bandw, false> from_general(const general_mat & gen){
   // Form a banded matrix from a general one, IFF bandw is sufficient
   int bandw_in = diag_bandw(gen);
   assert(bandw_in <= bandw); // Check it's storable!
@@ -577,7 +423,7 @@ banded_general<bandw> from_general(const general_mat & gen){
   return res;
 }
 
-using banded_general_penta = banded_general<5>;
-using penta_repeating = banded_repeating<5>;
+using banded_general_penta = banded_general<5, false>;
+using penta_repeating = banded_general<5, true>;
 
 #endif
