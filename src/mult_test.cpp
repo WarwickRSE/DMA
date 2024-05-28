6734be03dae395d9ba82bfaa4f26af171a18fdb1
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-05-28 12:27:59 +0100

    Better test example

--- src/mult_test.cpp
+++ src/mult_test.cpp
@@ -1,6 +1,7 @@
 
 #include "banded.hpp"
 #include <iostream>
+#include <cassert>
 
 int main(){
 
@@ -11,10 +12,12 @@ int main(){
   //Fill in
   for(int i = 0; i < len; i++){
     mat1_1.set(0, i, i);
-    mat1_2.set(0, i, -i);
   }
+  mat1_2 = mat1_1;
+  mat1_2.multiply_all(-1.0);
 
   auto mat1_3 = matmul(mat1_1, mat1_2);
+  std::cout<<"Multiplying two one-diagonal matrices\n";
   mat1_3.print();
   std::cout<<"______________________\n";
   std::cout<<"Result has bandwidth "<<diag_bandw(mat1_3)<<std::endl;
@@ -25,16 +28,16 @@ int main(){
   // Fill in
   for(int i = 0; i < len; i++){
       mat3_1.set(1, i, i+1);
-      mat3_2.set(1, i, -i-1);
       if(i < len-1){
           mat3_1.set(0, i, i+1);
-          mat3_2.set(0, i, -i-1);
           mat3_1.set(2, i, i+1);
-          mat3_2.set(2, i, -i-1);
       }
   }
+  mat3_2 = mat3_1;
+  mat3_2.multiply_all(-1.0);
 
   auto mat3_3 = matmul(mat3_1, mat3_2);
+  std::cout<<"Multiplying two tridiagonal matrices\n";
   mat3_3.print();
   std::cout<<"______________________\n";
   std::cout<<"Result has bandwidth "<<diag_bandw(mat3_3)<<std::endl;
@@ -45,26 +48,27 @@ int main(){
     // Fill in
     for(int i = 0; i < len; i++){
         mat5_1.set(2, i, i+1);
-        mat5_2.set(2, i, -i-1);
         if(i < len-1){
             mat5_1.set(1, i, i+1);
-            mat5_2.set(1, i, -i-1);
             mat5_1.set(3, i, i+1);
-            mat5_2.set(3, i, -i-1);
         }
         if(i < len-2){
             mat5_1.set(0, i, i+1);
-            mat5_2.set(0, i, -i-1);
             mat5_1.set(4, i, i+1);
-            mat5_2.set(4, i, -i-1);
         }
     }
+    mat5_2 = mat5_1;
+    mat5_2.multiply_all(-1.0);
 
-    std::cout<<"Equality checking test: "<<(mat5_1 == mat5_1)<<" "<< (mat5_1 == mat5_2)<<std::endl;
+    std::cout<<"Checking equality of matrices\n";
+    assert(mat5_1 != mat5_2);
+    assert(mat5_1 == mat5_1);
     auto mat5_1_copy = mat5_1.to_general();
-    std::cout<<"equality another way: "<<(mat5_1 == mat5_1_copy)<<std::endl;
+    assert(mat5_1 == mat5_1_copy);
+    std::cout<<"..... done\n";
 
     auto mat5_3 = matmul(mat5_1, mat5_2);
+    std::cout<<"Multiplying two penta-diagonal matrices\n";
     mat5_3.print();
     std::cout<<"______________________\n";
     std::cout<<"Result has bandwidth "<<diag_bandw(mat5_3)<<std::endl;
@@ -75,36 +79,40 @@ int main(){
     auto mat_e2 = mat5_2.to_general();
 
     auto mat_e3 = matmul(mat_e1, mat_e2);
-    std::cout<<"Result is the same? "<<(mat_e3 == mat5_3)<<std::endl;
-
+    std::cout<<"Checking banded and non banded multiplication\n";
+    assert(mat_e3 == mat5_3);
+    std::cout<<"..... done\n";
 
     //Looking at repeating matrices
     penta_repeating mat_repeating(len, 2, 2);
     mat_repeating.fill_from_elemBanded(elementary_matrices::SpringBanded);
 
     auto mat_repeating_expanded = mat_repeating.to_full();
-
+    std::cout<<"Creating a banded matrix from a repeating one\n";
     mat_repeating.print();
     std::cout<<"______________________\n";
     mat_repeating_expanded.print();
-
-    std::cout<<"Repeating matrix equality: "<<(mat_repeating == mat_repeating_expanded)<<std::endl;
+    assert(mat_repeating == mat_repeating_expanded);
+    std::cout<<"..... done\n";
 
     // Conversion to real matrix
     auto mat_repeating_general = mat_repeating.to_general();
+    std::cout<<"Now creating full square matrix\n";
     mat_repeating_general.print();
     std::cout<<"______________________\n";
     mat_repeating.expanded_print();
     std::cout<<"______________________\n";
+    assert(mat_repeating_general == mat_repeating_expanded);
+    std::cout<<"..... done\n";
  
     // Matmul on repeating
+    std::cout<<"Verifying multiplication with repeating matrices\n";
     auto mat_repeating_2 = matmul(mat_repeating, mat_repeating);
     mat_repeating_2.print();
     std::cout<<"______________________\n";
     auto mat_expanded_2 = matmul(mat_repeating_general, mat_repeating_general);
     mat_expanded_2.print();
-    std::cout<<"Results equal? "<<(mat_repeating_2 == mat_expanded_2)<<std::endl;
-
-    mat_repeating.len = 10;
+    assert(mat_repeating_2 == mat_expanded_2);
+    std::cout<<"..... done\n";
 
 }
