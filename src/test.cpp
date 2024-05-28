50a79d68dabb7a8819268fdfd7cdf744d9884f13
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2024-05-28 14:10:08 +0100

    Make test more clear

--- src/test.cpp
+++ src/test.cpp
@@ -19,30 +19,35 @@ penta_thomas_solver solver;
 solver.update_array(mat);
 std::cout<<"Verifying LU decomposition"<<std::endl;
 bool err = solver.verify_stored_array(mat);
-if(err) std::cout<<"Problem in decmposition"<<std::endl;
+if(err) std::cout<<"Problem in decomposition"<<std::endl;
 
 std::vector<double> rhs, sol;
 rhs.resize(len);
+sol.resize(len);
 for(int i=1; i<len; i++){
-    rhs[i] = rhs[i-1] + 0.1;
+    sol[i] = sol[i-1] + 0.1;
 }
-long n_iter = 1;
+long n_iter = 10;
 for(int t = 0; t<n_iter;t++){
+    // Do a bunch of solves, checking the result is within the expected error
     mat.clear();
     mat.fill_from_elemBanded(elementary_matrices::SpringBanded);
     mat.add_identity_factor(0.1);
     solver.update_array(mat);
-    rhs[0] += 0.1 * float(t)/float(n_iter);
+    for(int i=0; i<len; i++){
+      rhs[i] = sol[i] * 0.1;
+    }
     sol = solver.solve(rhs);
-    std::cout<<"Print to trick the optimiser: "<<sol[0]<<std::endl;
+    auto b = matvecmult(mat, sol);
+    double max_err = 0.0;
+    for(int i=0; i<len; i++){
+      if(std::abs(b[i] - rhs[i]) > max_err){
+        max_err = std::abs(b[i] - rhs[i]);
+      }
+    }
+    // Check ABSOLUTE error
+    std::cout<<"Iteration "<<t<<", error "<<max_err<<std::endl;
 }
-
-for(int i=0; i<len; i++) std::cout<<rhs[i]<<",";
-std::cout<<std::endl;
-
-for(int i=0; i<len; i++) std::cout<<sol[i]<<",";
-std::cout<<std::endl;
-
 
   // Try out the nudge resizing
   std::cout<<"Growing matrix \n";
@@ -54,10 +59,18 @@ std::cout<<std::endl;
   if(err) std::cout<<"Problem in decomposition "<<std::endl;
 
   rhs.resize(new_len);
-  for(int i=1; i<new_len; i++){
-    rhs[i] = rhs[i-1] + 0.1;
+  for(int i=1; i<len; i++){
+    rhs[i] =  rhs[i-1] + 0.01; // Same original rhs
   }
   sol = solver.solve(rhs);
-  std::cout<<sol[0]<<", "<<sol[new_len-1]<<std::endl;
+  auto b = matvecmult(mat, sol);
+  double max_err = 0.0;
+  for(int i=0; i<len; i++){
+    if(std::abs(b[i] - rhs[i]) > max_err){
+      max_err = std::abs(b[i] - rhs[i]);
+    }
+  }
+  // Check ABSOLUTE error
+  std::cout<<"Resized array, error "<<max_err<<std::endl;
 
 }
